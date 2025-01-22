75195cd9ba0067c55fecf01c106b38a8d06cd91f
Olexii_Omelchenko <oleksijomelcenko7@gmail.com>
2025-01-23 01:09:56 +0200

    main+additional

--- src/main/scala/kse/unit1/challenge/arithmetic.scala
+++ src/main/scala/kse/unit1/challenge/arithmetic.scala
@@ -4,7 +4,6 @@ import scala.annotation.tailrec
 
 object arithmetic:
 
-
   type Number = Long
 
   val increment: Number => Number =
@@ -25,19 +24,43 @@ object arithmetic:
       else -value
 
   def addition(left: Number, right: Number): Number =
-    require(left >= 0, "Left must be non-negative")
-    require(right >= 0, "Right must be non-negative")
+    // require(left >= 0, "Left must be non-negative")
+    // require(right >= 0, "Right must be non-negative")
 
-    ???
+    @tailrec
+    def saver(left: Number, right: Number): Number =
+      if isZero(left) then right
+      else if isZero(right) then left
+      else if (isNonNegative(left) && !isNonNegative(right)) || (!isNonNegative(left) && !isNonNegative(right))
+      then saver(decrement(left), increment(right))
+      else saver(increment(left), decrement(right))
+
+    saver(left, right)
 
   def multiplication(left: Number, right: Number): Number =
-    require(left >= 0, "Left must be non-negative")
-    require(right >= 0, "Right must be non-negative")
+    // require(left >= 0, "Left must be non-negative")
+    // require(right >= 0, "Right must be non-negative")
 
-    ???
+    @tailrec
+    def saver(left: Number, right: Number, acc: Number): Number =
+      if isZero(right) then acc
+      else if !isNonNegative(left) && isNonNegative(right)
+      then saver(left, decrement(right), addition(acc, left))
+      else if isNonNegative(left) && !isNonNegative(right)
+      then saver(right, decrement(left), addition(acc, right))
+      else if !isNonNegative(left) && !isNonNegative(right)
+      then saver(abs(left), decrement(abs(right)), addition(acc, abs(left)))
+      else saver(left, decrement(right), addition(acc, left))
+
+    saver(left, right, 0)
 
   def power(base: Number, p: Number): Number =
     require(p >= 0, "Power must be non-negative")
     require(base != 0 || p != 0, "0^0 is undefined")
 
-    ???
+    @tailrec
+    def saver(base: Number, p: Number, acc: Number): Number =
+      if isZero(p) then acc
+      else saver(base, decrement(p), multiplication(acc, base))
+
+    saver(base, p, 1)
