8eacd63907492576fc8cf0c067fb0a5696520d2a
Olexii_Omelchenko <oleksijomelcenko7@gmail.com>
2025-02-15 16:13:49 +0200

    reverted

---
+++ src/main/scala/kse/unit1/challenge/arithmetic.scala
@@ -0,0 +1,62 @@
+package kse.unit1.challenge
+
+import scala.annotation.tailrec
+
+object arithmetic:
+
+  type Number = Long
+
+  val increment: Number => Number =
+    value => value + 1
+
+  val decrement: Number => Number =
+    value => value - 1
+
+  val isZero: Number => Boolean =
+    value => value == 0
+
+  val isNonNegative: Number => Boolean =
+    value => value >= 0
+
+  val abs: Number => Number =
+    value =>
+      if isNonNegative(value) then value
+      else -value
+
+  def addition(left: Number, right: Number): Number =
+
+    @tailrec
+    def saver(left: Number, right: Number): Number =
+      if isZero(left) then right
+      else if isZero(right) then left
+      else if (isNonNegative(left) && !isNonNegative(right)) || (!isNonNegative(left) && !isNonNegative(right))
+      then saver(decrement(left), increment(right))
+      else saver(increment(left), decrement(right))
+
+    saver(left, right)
+
+  def multiplication(left: Number, right: Number): Number =
+
+    @tailrec
+    def saver(left: Number, right: Number, acc: Number): Number =
+      if isZero(right) then acc
+      else if !isNonNegative(left) && isNonNegative(right)
+      then saver(left, decrement(right), addition(acc, left))
+      else if isNonNegative(left) && !isNonNegative(right)
+      then saver(right, decrement(left), addition(acc, right))
+      else if !isNonNegative(left) && !isNonNegative(right)
+      then saver(abs(left), decrement(abs(right)), addition(acc, abs(left)))
+      else saver(left, decrement(right), addition(acc, left))
+
+    saver(left, right, 0)
+
+  def power(base: Number, p: Number): Number =
+    require(p >= 0, "Power must be non-negative")
+    require(base != 0 || p != 0, "0^0 is undefined")
+
+    @tailrec
+    def saver(base: Number, p: Number, acc: Number): Number =
+      if isZero(p) then acc
+      else saver(base, decrement(p), multiplication(acc, base))
+
+    saver(base, p, 1)
---
+++ src/test/scala/kse/unit1/challenge/ArithmeticSpecification.scala
@@ -0,0 +1,37 @@
+package kse.unit1.challenge
+
+import kse.unit1.challenge.arithmetic.*
+import kse.unit1.challenge.generators.{genSmallNonNegativeNumber, genSmallNumber}
+import org.scalacheck.*
+import org.scalacheck.Prop.{forAll, propBoolean}
+
+object ArithmeticSpecification extends Properties("Arithmetic"):
+
+  include(AdditionSpecification)
+  include(MultiplicationSpecification)
+  include(PowerSpecification)
+
+end ArithmeticSpecification
+
+object AdditionSpecification extends Properties("Addition"):
+
+  property("left + right should be correctly evaluated") = forAll(genSmallNumber, genSmallNumber): (left: Number, right: Number) =>
+    addition(left, right) == left + right
+
+end AdditionSpecification
+
+object MultiplicationSpecification extends Properties("Multiplication"):
+
+  property("left * right should be correctly evaluated") = forAll(genSmallNumber, genSmallNumber): (left: Number, right: Number) =>
+    multiplication(left, right) == left * right
+
+end MultiplicationSpecification
+
+object PowerSpecification extends Properties("Power"):
+
+  property("base ^ p should be correctly evaluated") = forAll(genSmallNumber, genSmallNonNegativeNumber): (base: Number, p: Number) =>
+    (base != 0 && p != 0) ==> {
+      power(base, p) == Math.pow(base, p).toLong
+    }
+
+end PowerSpecification
