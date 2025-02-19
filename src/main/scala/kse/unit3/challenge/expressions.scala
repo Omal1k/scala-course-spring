7d41d329741f3b49a0acecf179d7fd94612453e1
Olexii_Omelchenko <oleksijomelcenko7@gmail.com>
2025-02-20 01:23:58 +0200

    added negation, conjunction, disjunction

--- src/main/scala/kse/unit3/challenge/expressions.scala
+++ src/main/scala/kse/unit3/challenge/expressions.scala
@@ -1,5 +1,6 @@
 package kse.unit3.challenge
 
+import java.beans.Expression
 import scala.annotation.{tailrec, targetName}
 
 object expressions:
@@ -9,8 +10,8 @@ object expressions:
     def substitute(variable: Variable, expression: Expression): Expression
 
   sealed trait Boolean extends Expression:
-    val evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
+    val evaluate: Expression                                                 = this
+    def substitute(variable: Variable, substitution: Expression): Expression = this
 
   type True = True.type
   case object True extends Boolean
@@ -19,23 +20,44 @@ object expressions:
   case object False extends Boolean
 
   case class Variable(name: String) extends Expression:
-    val evaluate: Expression                                               = ???
-    def substitute(variable: Variable, expression: Expression): Expression = ???
+    val evaluate: Expression = this
+
+    def substitute(variable: Variable, expression: Expression): Expression =
+      if this == Variable then expression
+      else this
 
   case class Negation(expression: Expression) extends Expression:
-    def evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
-    override def toString: String                                            = ???
+
+    lazy val evaluate: Expression = expression.evaluate match
+      case True  => False
+      case False => True
+      case other => Negation(other)
+
+    def substitute(variable: Variable, substitution: Expression): Expression =
+      Negation(expression.substitute(variable, substitution))
+    override def toString: String = s"!$expression"
 
   case class Conjunction(left: Expression, right: Expression) extends Expression:
-    def evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
-    override def toString: String                                            = ???
+
+    lazy val evaluate: Expression = (left.evaluate, right.evaluate) match
+      case (True, True)            => True
+      case (False, _) | (_, False) => False
+      case (left, right)           => Conjunction(left, right)
+
+    def substitute(variable: Variable, substitution: Expression): Expression =
+      Conjunction(left.substitute(variable, substitution), right.substitute(variable, substitution))
+    override def toString: String = s"$left ∧ $right"
 
   case class Disjunction(left: Expression, right: Expression) extends Expression:
-    def evaluate: Expression                                                 = ???
-    def substitute(variable: Variable, substitution: Expression): Expression = ???
-    override def toString: String                                            = ???
+
+    lazy val evaluate: Expression = (left.evaluate, right.evaluate) match
+      case (True, _) | (_, True) => True
+      case (False, False)        => False
+      case (left, right)         => Disjunction(left, right)
+
+    def substitute(variable: Variable, substitution: Expression): Expression =
+      Disjunction(left.substitute(variable, substitution), right.substitute(variable, substitution))
+    override def toString: String = s"$left ∨ $right"
 
   case class Implication(left: Expression, right: Expression) extends Expression:
     def evaluate: Expression                                                 = ???
