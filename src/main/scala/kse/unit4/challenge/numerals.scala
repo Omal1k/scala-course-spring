d9b3bfc511554dffaef2cf0792fa191d50434825
Olexii_Omelchenko <oleksijomelcenko7@gmail.com>
2025-03-03 19:53:30 +0200

    task done

--- src/main/scala/kse/unit4/challenge/numerals.scala
+++ src/main/scala/kse/unit4/challenge/numerals.scala
@@ -10,19 +10,19 @@ object numerals:
 
     def predecessor: Numeral
 
-    def successor: Numeral = ???
+    def successor: Numeral = Successor(this)
 
     @targetName("greater than")
     infix def >(that: Numeral): Boolean
 
     @targetName("greater or equal to")
-    infix def >=(that: Numeral): Boolean = ???
+    infix def >=(that: Numeral): Boolean = (this == that) || (this > that)
 
     @targetName("less than")
-    infix def <(that: Numeral): Boolean = ???
+    infix def <(that: Numeral): Boolean = !(this >= that)
 
     @targetName("less or equal to")
-    infix def <=(that: Numeral): Boolean = ???
+    infix def <=(that: Numeral): Boolean = !(this > that)
 
     @targetName("addition")
     infix def +(that: Numeral): Numeral
@@ -39,47 +39,56 @@ object numerals:
 
   object Zero extends Numeral:
 
-    def isZero: Boolean = ???
+    def isZero: Boolean = true
 
-    def predecessor: Numeral = ???
+    def predecessor: Numeral = throw new NoSuchElementException("Zero does not have predecessor in church numerals")
 
     @targetName("greater than")
-    infix def >(that: Numeral): Boolean = ???
+    infix def >(that: Numeral): Boolean = false
 
     @targetName("addition")
-    infix def +(that: Numeral): Numeral = ???
+    infix def +(that: Numeral): Numeral = that
 
     // Optional
     @targetName("subtraction")
-    infix def -(that: Numeral): Numeral = ???
+    infix def -(that: Numeral): Numeral = that match
+      case Zero => this
+      case _    => throw new UnsupportedOperationException("Cannot substract from 0 in church numerals")
 
-    def toInt: Int = ???
+    def toInt: Int = 0
 
-    override def toString: String = ???
+    override def toString: String = s"Nat($toInt)"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean = obj match
+      case value: Zero.type => true
+      case _                => false
 
   object Successor:
     def unapply(successor: Successor): Option[Numeral] = Option(successor.predecessor)
 
   class Successor(n: Numeral) extends Numeral:
 
-    def isZero: Boolean = ???
+    def isZero: Boolean = false
 
-    def predecessor: Numeral = ???
+    def predecessor: Numeral = n
 
     @targetName("greater than")
-    infix def >(that: Numeral): Boolean = ???
+    infix def >(that: Numeral): Boolean = that match
+      case Zero => false
 
     @targetName("addition")
-    infix def +(that: Numeral): Numeral = ???
+    infix def +(that: Numeral): Numeral = Successor(n + that)
 
     // Optional
     @targetName("subtraction")
-    infix def -(that: Numeral): Numeral = ???
+    infix def -(that: Numeral): Numeral = that match
+      case value if value.isZero => this
+      case s: Successor          => this.predecessor - s.predecessor
 
     def toInt: Int = 1 + n.toInt
 
-    override def toString: String = ???
+    override def toString: String = s"Nat($toInt)"
 
-    override def equals(obj: Any): Boolean = ???
+    override def equals(obj: Any): Boolean = obj match
+      case s: Successor => this.predecessor == s.predecessor
+      case _            => false
--- src/test/scala/kse/unit4/challenge/NumeralsSpecification.scala
+++ src/test/scala/kse/unit4/challenge/NumeralsSpecification.scala
@@ -11,4 +11,31 @@ object NumeralsSpecification extends Properties("Numerals"):
   override def overrideParameters(p: Parameters): Parameters =
     p.withMinSuccessfulTests(50).withMaxDiscardRatio(100)
 
+  property("Zero == Zero") = (Zero == Zero)
+
+  property("Zero < value") = forAll: (value: Numeral) =>
+    if value.isZero then (Zero < value) == false
+    else (Zero < value) == true
+
+  property("succesor(n) == n+1") = forAll: (value: Numeral) =>
+    value.successor.toInt == value.toInt + 1
+
+  property("value + 0 == value") = forAll: (value: Numeral) =>
+    value + Zero == value
+
+  property("left + right == right + left") = forAll: (left: Numeral, right: Numeral) =>
+    left + right == right + left
+
+  property("a+(b+c) == (a+b)+c") = forAll: (a: Numeral, b: Numeral, c: Numeral) =>
+    (a + (b + c)).toInt == ((a + b) + c).toInt
+
+  property("reflexivity") = forAll: (left: Numeral, right: Numeral) =>
+    (left.toInt == right.toInt) == (left == right)
+
+  property("left - right == left - right") = forAll: (left: Numeral, right: Numeral) =>
+    try (left.toInt - right.toInt) == (left - right).toInt
+    catch case e: UnsupportedOperationException => true
+
+  property("value - zero == value") = forAll: (value: Numeral) =>
+    (value - Zero) == value
 end NumeralsSpecification
