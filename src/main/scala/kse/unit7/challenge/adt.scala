324f529062b8591b16cee66dd104b9929a81990f
Olexii_Omelchenko <oleksijomelcenko7@gmail.com>
2025-04-05 21:56:01 +0300

    did main task

--- src/main/scala/kse/unit7/challenge/adt.scala
+++ src/main/scala/kse/unit7/challenge/adt.scala
@@ -4,11 +4,23 @@ object adt:
 
   enum Try[+V]:
 
-    case DummyCase
+    case Success(value: V)         extends Try[V]
+    case Failure(error: Throwable) extends Try[Nothing]
 
-    def flatMap[Q](f: V => Try[Q]): Try[Q] = ???
+    def flatMap[Q](f: V => Try[Q]): Try[Q] =
+      this match
+        case Failure(error) => Failure(error)
+        case Success(value) => f(value)
 
-    def map[Q](f: V => Q): Try[Q] = ???
+    def map[Q](f: V => Q): Try[Q] =
+      this match
+        case Failure(error) => Failure(error)
+        case Success(value) =>
+          try Success(f(value))
+          catch case error: Throwable => Failure(error)
 
   object Try:
-    def apply[V](v: V): Try[V] = ???
+
+    def apply[V](v: V): Try[V] =
+      try Success(v)
+      catch case error: Throwable => Failure(error)
--- src/main/scala/kse/unit7/challenge/app.scala
+++ src/main/scala/kse/unit7/challenge/app.scala
@@ -6,10 +6,43 @@ import kse.unit7.challenge.services.*
 
 object app:
 
-  def getPostsViews(apiKey: ApiKey): Try[List[PostView]] = ???
+  def getPostsViews(apiKey: ApiKey): Try[List[PostView]] =
+    for
+      user  <- getUserProfile(apiKey)
+      posts <- getPosts(user.userId)
+      views <- posts.foldLeft(Try(List.empty[PostView])) { (cSave, post) =>
+        for
+          saver <- cSave
+          view  <- getPostView(post)
+        yield saver :+ view
+      }
+    yield views
 
-  def getPostsViewDesugared(apiKey: ApiKey): Try[List[PostView]] = ???
+  def getPostsViewDesugared(apiKey: ApiKey): Try[List[PostView]] =
+    getUserProfile(apiKey).flatMap { profile =>
+      getPosts(profile.userId).flatMap { posts =>
+        posts.foldLeft(Try(List.empty)) { (cSave, post) =>
+          cSave.flatMap { saver =>
+            getPostView(post).map { view =>
+              saver :+ view
+            }
+          }
+        }
+      }
+    }
 
-  def getPostView(post: Post): Try[PostView] = ???
+  def getPostView(post: Post): Try[PostView] =
+    for
+      comments <- getComments(post.postId)
+      likes    <- getLikes(post.postId)
+      shares   <- getShares(post.postId)
+    yield PostView(post, comments, likes, shares)
 
-  def getPostViewDesugared(post: Post): Try[PostView] = ???
+  def getPostViewDesugared(post: Post): Try[PostView] =
+    getComments(post.postId).flatMap { comments =>
+      getLikes(post.postId).flatMap { likes =>
+        getShares(post.postId).map { shares =>
+          PostView(post, comments, likes, shares)
+        }
+      }
+    }
